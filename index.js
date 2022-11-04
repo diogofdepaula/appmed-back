14ca7928f4b682405fc346a224ac7d1c134363e3
diogofdepaula <diogofdepaula@gmail.com>
2022-11-04 15:43:58 -0300

    Atualização para react 18

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 
-// const corsopt = {
-//     origin: '*',
-//     credentials: true,
-//     optionSuccessStatus: 200
-// }
-
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
